e828fbb6b7f502e9f8f4ee2e9740ea82327a6f95
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-06-16 13:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const btnElem = document.querySelector(".btn");
+const modalContainer = document.getElementById("modal");
+
+btnElem.addEventListener("click",(e)=>{
+	if(modalContainer.classList.contains("hide")){
+		modalContainer.classList.remove("hide");
+	}else{
+		
+		modalContainer.classList.add("hide");
+	}
+	
+})
