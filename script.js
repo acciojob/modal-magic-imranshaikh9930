a078f26f9f950626788c84cf811d4bfa5bff97b9
imran <87297004+imranshaikh9930@users.noreply.github.com>
2025-06-16 13:44:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //your JS code here. If required.
-const btnElem = document.querySelector(".btn");
+const btnElem = document.querySelector("#openModal");
 const modalContainer = document.getElementById("modal");
 
 btnElem.addEventListener("click",(e)=>{
 	if(modalContainer.classList.contains("hide")){
+		   document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.2)'
+		modalContainer.style.backgroundColor = "white";
 		modalContainer.classList.remove("hide");
 	}else{
-		
+		   document.body.style.backgroundColor = 'transparent'
 		modalContainer.classList.add("hide");
 	}
 	
